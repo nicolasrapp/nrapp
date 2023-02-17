37228c2b6fe51c9fa4a6ae59224c8f25db8517a3
nicorapp <nicolas.rapp@epfedu.fr>
2023-02-17 15:07:52 +0100

    modifs du main

--- src/main/java/org/oxyl/Main.java
+++ src/main/java/org/oxyl/Main.java
@@ -3,6 +3,8 @@ package org.oxyl;
 public class Main {
     public static void main(String [] args){
         System.out.println("It works");
+        System.out.println("jaime");
+
 
         Cercle cercle1 = new Cercle(10, 10, 10);
         System.out.println(cercle1);
